c47556c6c43bf57fd29e64227e6fb53d9ffa33fe
Ayush Likhar <neontechhead@gmail.com>
2019-05-06 08:03:02 +0530

    boards and topics

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponse
 
+from .models import Board
+
 def home(request):
-    return HttpResponse('Hello, World!')
+		boards = Board.objects.all()
+		return render(request, 'home.html', {'boards': boards})
+
+# def board_topics(request, pk):
+#     try:
+#         board = Board.objects.get(pk=pk)
+#     except Board.DoesNotExist:
+#         raise Http404
+#     return render(request, 'topics.html', {'board': board})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})    
